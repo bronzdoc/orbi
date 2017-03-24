2b3143dfbb318efa6e3789f039375f5f8320be56
bronzdoc <lsagastume1990@gmail.com>
2017-03-23 21:59:33 -0600

    Add template content to file

--- skeleton/actions/create.go
+++ skeleton/actions/create.go
@@ -1,8 +1,9 @@
 package actions
 
 import (
-	"fmt"
+	_ "fmt"
 	"github.com/bronzdoc/skeletor/template"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -16,21 +17,36 @@ func Create(template template.Template) {
 			if key == "dir" {
 				data := val.(map[interface{}]interface{})
 				dir_name := data["name"]
-				fmt.Println(dir_name)
 				os.Mkdir(context+"/"+dir_name.(string), 0777)
 				if _, ok := data["files"]; ok {
 					files := data["files"].([]interface{})
 					for j := range files {
 						filename := context + "/" + dir_name.(string) + "/" + files[j].(string)
-						fmt.Println(filename)
-						f, err := os.Create(filename)
-						if err != nil {
-							log.Fatal(err)
-						}
+						f := create_file(filename)
 						defer f.Close()
+						template_name := os.Getenv("HOME") + "/" + ".skeletor/templates" + "/" + files[j].(string)
+						if _, err := os.Stat(template_name); err == nil {
+							add_template_content_to_file(template_name, f)
+						}
 					}
 				}
 			}
 		}
 	}
+}
+
+func create_file(filename string) *os.File {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func add_template_content_to_file(template_name string, file *os.File) {
+	template_content, err := ioutil.ReadFile(template_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file.Write(template_content)
 }
