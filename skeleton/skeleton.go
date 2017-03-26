ba54b9dde25087932c088fc635b6981a3dcf7b37
bronzdoc <lsagastume1990@gmail.com>
2017-03-26 17:05:37 -0600

    Restructured Skeleton

--- skeleton/skeleton.go
+++ skeleton/skeleton.go
@@ -1,20 +1,68 @@
 package skeleton
 
 import (
-	"github.com/bronzdoc/skeletor/skeleton/actions"
 	"github.com/bronzdoc/skeletor/template"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 type skeleton struct {
-	template template.Template
+	Context   string
+	Resources []map[string]interface{}
+	vars      map[string]string
 }
 
-func New(template template.Template) *skeleton {
-	return &skeleton{
-		template: template,
+func New(skeleton_file string, vars map[string]string) *skeleton {
+	sk := skeleton{}
+	sk.vars = vars
+	data, err := ioutil.ReadFile(skeleton_file)
+
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	if err = yaml.Unmarshal(data, &sk); err != nil {
+		log.Fatal(err)
+	}
+
+	return &sk
 }
 
 func (s *skeleton) Create() {
-	actions.Create(s.template)
+	resources := s.Resources
+	context := s.Context
+
+	for i := range resources {
+		for key, val := range resources[i] {
+			if key == "dir" {
+				data := val.(map[interface{}]interface{})
+				dir_name := data["name"]
+				os.Mkdir(context+"/"+dir_name.(string), 0777)
+				if _, ok := data["files"]; ok {
+					files := data["files"].([]interface{})
+					for j := range files {
+						filename := files[j].(string)
+						file_path := context + "/" + dir_name.(string) + "/" + filename
+						f := create_file(file_path)
+						defer f.Close()
+						template_path := os.Getenv("HOME") + "/" + ".skeletor/templates" + "/" + filename
+						_, err := template.New(filename, template_path).Create(f, s.vars)
+						if err != nil {
+							log.Fatal(err)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func create_file(filename string) *os.File {
+	f, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
 }
