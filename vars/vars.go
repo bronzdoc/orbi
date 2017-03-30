c39bb3c73087ba98a94c6ae197933b1b8e7a80fa
bronzdoc <lsagastume1990@gmail.com>
2017-03-29 21:36:11 -0600

    Make parse delegates error

--- vars/vars.go
+++ vars/vars.go
@@ -1,23 +1,23 @@
 package vars
 
 import (
-	"log"
 	"regexp"
 	"strings"
 )
 
-func Parse(str string) map[string]string {
+func Parse(str string) (map[string]string, error) {
+	data := make(map[string]string)
 	regex, err := regexp.Compile(`\s?[a-zA-Z]+\s?=\s?\w+`)
+
 	if err != nil {
-		log.Fatal(err)
+		return data, err
 	}
 
-	data := make(map[string]string)
 	matches := regex.FindAllString(str, -1)
 	for _, match := range matches {
 		s := strings.Split(match, "=")
 		key, value := strings.TrimSpace(s[0]), strings.TrimSpace(s[1])
 		data[key] = value
 	}
-	return data
+	return data, nil
 }
