c9a224770e118db62b58b61873b0088d4832851c
bronzdoc <lsagastume1990@gmail.com>
2017-04-13 21:57:11 -0600

    Delegate resource creation to each resource node

--- definition/definition.go
+++ definition/definition.go
@@ -1,68 +1,39 @@
 package definition
 
 import (
-	"github.com/bronzdoc/symbiote/template"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
-	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type definition struct {
-	Context   string
-	Resources []map[string]interface{}
-	vars      map[string]string
+	Context      string
+	Resources    []map[interface{}]interface{}
+	ResourceTree *tree
+	Options      map[string]interface{}
 }
 
-func New(definition_file string, vars map[string]string) *definition {
-	def := definition{}
-	def.vars = vars
-	data, err := ioutil.ReadFile(definition_file)
+func New(definition_file string, options map[string]interface{}) *definition {
+	def := definition{Options: options}
 
+	definition_content, err := ioutil.ReadFile(definition_file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err = yaml.Unmarshal(data, &def); err != nil {
+	if err = yaml.Unmarshal(definition_content, &def); err != nil {
 		log.Fatal(err)
 	}
+
+	def.ResourceTree = newTree(def.Context, def.Resources)
 
 	return &def
 }
 
-func (s *definition) Create() {
-	resources := s.Resources
-	context := s.Context
-
-	for i := range resources {
-		for key, val := range resources[i] {
-			if key == "dir" {
-				data := val.(map[interface{}]interface{})
-				dir_name := data["name"]
-				os.Mkdir(context+"/"+dir_name.(string), 0777)
-				if _, ok := data["files"]; ok {
-					files := data["files"].([]interface{})
-					for j := range files {
-						filename := files[j].(string)
-						file_path := context + "/" + dir_name.(string) + "/" + filename
-						f := create_file(file_path)
-						defer f.Close()
-						template_path := os.Getenv("HOME") + "/" + ".definitions/templates" + "/" + filename
-						_, err := template.New(filename, template_path, s.vars).Create(f)
-						if err != nil {
-							log.Fatal(err)
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-func create_file(filename string) *os.File {
-	f, err := os.Create(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return f
+func (d *definition) Create() {
+	tree := d.ResourceTree
+	tree.Traverse(func(node Resource) {
+		node.Create(d.Options)
+	})
 }
