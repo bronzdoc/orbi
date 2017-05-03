de4faf4abcd55cb4a46e96f2a510e89ff59112e0
bronzdoc <lsagastume1990@gmail.com>
2017-05-02 22:30:27 -0600

    Add default config using viper

--- cmd/root.go
+++ cmd/root.go
@@ -30,11 +30,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports Persistent Flags, which, if defined here,
 	// will be global for your application.
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.orbi/config.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.orbi/.config.yaml)")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -43,9 +39,13 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	}
 
-	viper.SetConfigName(".config") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")   // adding home directory as first search path
-	viper.AutomaticEnv()           // read in environment variables that match
+	viper.SetDefault("OrbiPath", fmt.Sprintf("%s/.orbi", os.Getenv("HOME")))
+	viper.SetDefault("PlansPath", fmt.Sprintf("%s/plans", viper.GetString("OrbiPath")))
+	viper.SetDefault("TemplatesDir", "templates")
+
+	viper.SetConfigName(".config")     // name of config file (without extension)
+	viper.AddConfigPath("$HOME/.orbi") // adding home directory as first search path
+	viper.AutomaticEnv()               // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
