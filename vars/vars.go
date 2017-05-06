6862a008881104f2d2394dde8e7add75a92fa8c7
bronzdoc <lsagastume1990@gmail.com>
2017-05-06 15:11:42 -0600

    Add Vars test suite

--- vars/vars.go
+++ vars/vars.go
@@ -5,20 +5,20 @@ import (
 	"strings"
 )
 
-// Parse parses a string to look for vars
+// Parse parses a vars string i.e "var_name=value"
 func Parse(str string) (map[string]string, error) {
-	data := make(map[string]string)
-	regex, err := regexp.Compile(`\s?[a-zA-Z]+\s?=\s?\w+`)
+	vars := make(map[string]string)
+	regex, err := regexp.Compile(`\s?[a-zA-Z_.-]+\s*=\s*.+`)
 
 	if err != nil {
-		return data, err
+		return vars, err
 	}
 
 	matches := regex.FindAllString(str, -1)
 	for _, match := range matches {
 		s := strings.Split(match, "=")
 		key, value := strings.TrimSpace(s[0]), strings.TrimSpace(s[1])
-		data[key] = value
+		vars[key] = value
 	}
-	return data, nil
+	return vars, nil
 }
---
+++ vars/vars_test.go
@@ -0,0 +1,38 @@
+package vars_test
+
+import (
+	"reflect"
+
+	. "github.com/bronzdoc/orbi/vars"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Vars", func() {
+	Describe("Parse", func() {
+		It("Should parse a string with vars correctly", func() {
+			strvars := `
+			ninja_turtle=donatello
+			logan = wolverine
+			gandalf= magneto
+			cat-dog = nickelodeon
+			homer.simpson=Doh!
+			`
+
+			expectedMap := map[string]string{
+				"ninja_turtle":  "donatello",
+				"logan":         "wolverine",
+				"gandalf":       "magneto",
+				"cat-dog":       "nickelodeon",
+				"homer.simpson": "Doh!",
+			}
+
+			vars, err := Parse(strvars)
+			Expect(err).To(BeNil())
+
+			eql := reflect.DeepEqual(vars, expectedMap)
+			Expect(eql).To(BeTrue())
+		})
+	})
+})
