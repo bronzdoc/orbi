fa8d0dc641bc7c53fc656a9c1388f4a4f01812d1
bronzdoc <lsagastume1990@gmail.com>
2017-05-06 18:05:32 -0600

    Create ~/.orbi directory tree if missing when root command runs

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"os"
 )
 
 // RootCmd represents the base command when called without any subcommands
@@ -13,13 +14,22 @@ var RootCmd = &cobra.Command{
 	Short: `Project structure generator`,
 }
 
+var orbiPath = fmt.Sprintf("%s/.orbi", os.Getenv("HOME"))
+var plansPath = fmt.Sprintf("%s/plans", orbiPath)
+
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	// Create ~/.orbi directory and sub directories if missing
+	if !pathExists(orbiPath) || !pathExists(plansPath) {
+		os.MkdirAll(plansPath, 0776)
+	}
+
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+
 }
 
 func init() {
@@ -28,10 +38,15 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	viper.SetDefault("OrbiPath", fmt.Sprintf("%s/.orbi", os.Getenv("HOME")))
-	viper.SetDefault("PlansPath", fmt.Sprintf("%s/plans", viper.GetString("OrbiPath")))
+	viper.SetDefault("OrbiPath", orbiPath)
+	viper.SetDefault("PlansPath", plansPath)
 	viper.SetDefault("TemplatesDir", "templates")
 
 	viper.SetConfigName(".config")     // name of config file (without extension)
 	viper.AddConfigPath("$HOME/.orbi") // adding home directory as first search path
+}
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
 }
